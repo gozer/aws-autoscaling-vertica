13af14a2647eafc8052d3fa786ea5d3aa6897dc9
Bob Strahan <bob.strahan@hp.com>
2015-07-09 17:44:00 -0400

    updates

--- resume_cluster.sh
+++ resume_cluster.sh
@@ -6,9 +6,16 @@
 
 echo "Start all stopped node instances in cluster"
 instanceIds=$(aws --output=text ec2 describe-instances --filters Name=tag-key,Values=Name,Name=tag-value,Values=$autoscaling_group_name Name=instance-state-code,Values=80 --query "Reservations[*].Instances[*].InstanceId")
-aws ec2 start-instances --instance-id $instanceIds
+
+if [ -z "$instanceIds" ]; then
+   echo "There are no stopped instances with cluster name [$autoscaling_group_name]"
+else
+   echo "Starting instances [$instanceIds]"
+   aws ec2 start-instances --instance-id $instanceIds
+fi
 
 
+instanceIds=$(aws --output=text ec2 describe-instances --filters Name=tag-key,Values=Name,Name=tag-value,Values=$autoscaling_group_name --query "Reservations[*].Instances[*].InstanceId")
 echo "Wait for all cluster instances to be in running state"
 for instId in $instanceIds
 do
@@ -24,13 +31,15 @@ done
 echo "Get public IP address of first instance"
 publicIp=$(aws --output=text ec2 describe-instances --filters Name=tag-key,Values=Name,Name=tag-value,Values=$autoscaling_group_name --query "Reservations[*].Instances[*].PublicIpAddress" | head -1 | cut -f1)
 
-echo "Start database [$database_name] on node [$publicIp]"
+# wait till instance is accepting connections
 while [ 1 ]; do
-   ssh -i $pem_file -o "StrictHostKeyChecking no" dbadmin@$publicIp admintools -t start_db -d $database_name 
+   ssh -i $pem_file -o "StrictHostKeyChecking no" dbadmin@$publicIp echo "test ssh connection" > /dev/null
    [ $? -eq 0 ] && break
    echo "Waiting another 60s for instance to accept connections, and try again"
    sleep 60
 done
+echo "Start database [$database_name] on node [$publicIp]"
+ssh -i $pem_file -o "StrictHostKeyChecking no" dbadmin@$publicIp admintools -t start_db -d $database_name 
 
 echo "Resume autoscaling group processing"
 aws autoscaling resume-processes --auto-scaling-group-name $autoscaling_group_name
