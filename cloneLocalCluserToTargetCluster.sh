10daca449773af2f1ba0f60fa112a9ffb7013a68
Bob Strahan <bob.strahan@hp.com>
2015-07-10 18:47:05 -0400

    add autoscaling objects to a copied database

--- cloneLocalCluserToTargetCluster.sh
+++ cloneLocalCluserToTargetCluster.sh
@@ -121,7 +121,7 @@ echo "Start database on remote cluster"
 ssh $publicIp "admintools -t start_db -d $database_name"
 
 echo "Install autoscaling on target database"
-ssh $publicIp "cd /home/dbadmin/autoscale; sh database_configure.sh" > /dev/null >2&1
+ssh $publicIp "cd /home/dbadmin/autoscale; sh database_configure.sh > /dev/null 2>&1"
 
 echo "Done"
 exit 0
