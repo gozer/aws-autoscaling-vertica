8a4bd3e418a3a0580840ec5725a2c00e1d272ad2
Bob Strahan <bob.strahan@hp.com>
2015-07-17 10:51:04 -0400

    Complete alignment of db name. node names, paths

--- copyLocalClusterToTargetCluster.sh
+++ copyLocalClusterToTargetCluster.sh
@@ -1,13 +1,15 @@
 #! /bin/sh
 # Copyright (c) 2011-2015 by Vertica, an HP Company.  All rights reserved.
 
-# Setup and run copycluster from local cluster to the target autoscaling cluster
-# Target autoscaling cluster must be identified by 'autoscaling_group_name' in the autoscaling_vars.sh file
-# in the local directory. This is usually acomplished by creating the target cluster from a node on the source cluster.
-# The target cluster size MUST be the same as the local cluster size. 
-# The target cluster database name and password should be the same as the local cluster
+# This script will attempt to copy a local database to a remote AWS cluster.
+# 1. The remote AWS cluster must be configured from a node on the local cluster, using the aws-autoscaling-vertica package
+# 2. The remote cluster must be scaled to be the same node count as the local cluster
+# 4. The path used for data and catalog files will be replicated on the remote cluster
+#     - if the paths do not exist on remote cluster nodes, they will be created as symblic links to '/vertica/data'
+#     - There must be sufficient disk space on the remote cluster nodes
 
-
+# If local and remote database name, node names or paths do not align, then the database will be 
+# automatically recreated on the remote cluster with name, node names and paths matching the source cluster.
 
 
 . ./autoscaling_vars.sh
@@ -19,7 +21,6 @@ user=dbadmin
 userArg="-U $user"
 pwd="$password"
 pwdArg="-w $pwd"
-dbPath=/vertica/data  # constant for target database on AWS AMI
 
 function show_help {
    echo "$0 [-w dbadminPasswd]"
@@ -56,20 +57,6 @@ function get_instance_by_ip() {
 }
 
 
-# Find active local database name
-localdb=$(admintools -t show_active_db)
-if [ -z "$localdb" ]; then
-   echo "No local database running. Please start database, and try again."
-   exit 1
-else
-   echo "Active local database: $localdb"
-fi
-if [ $localdb != $database_name ]; then
-   echo "Local database name => $localdb"
-   echo "Target database name => $database_name"
-   echo "Target auto scaling cluster [$autoscaling_group_name] database name and dbadmin password must match local database"
-   exit 1
-fi
 
 # get Public IP address for remote cluster
 echo "Get public IP address of an instance in remote cluster"
@@ -92,39 +79,117 @@ fi
 echo "Local and Remote clusters have $local_node_count nodes"
 
 # capture local and remote node info
+database_name=$(admintools -t show_active_db)
+remote_db=$(ssh $publicIp admintools -t show_active_db)
+if [ -z "$database_name" ]; then
+   echo "No local database running. Please start database, and try again."
+   exit 1
+fi
 local_node_names=($(vsql $pwdArg -qAt -c "select node_name from nodes order by node_name"))
 remote_node_names=($(vsql -h $publicIp $pwdArg -qAt -c "select node_name from nodes order by node_name"))
 remote_node_addrs=($(vsql -h $publicIp $pwdArg -qAt -c "select node_address from nodes order by node_name"))
+local_db_paths=$(cat /opt/vertica/config/admintools.conf | egrep "^${local_node_names[0]} " | awk -F, '{printf "%s %s", $2, $3}')
+remote_db_paths=$(ssh $publicIp cat /opt/vertica/config/admintools.conf | egrep "^${remote_node_names[0]} " | awk -F, '{printf "%s %s", $2, $3}')
 
-# configure database node names on remote cluster if names don't align
+# recreate remote database if database name,  node names or paths don't match
+recreate_remote_db=0
+if [ "$database_name" != "$remote_db" ]; then
+   echo "Local database name => $database_name"
+   echo "Remote database name => $remote_db"
+   echo "Database [$database_name] will be recreated on the target cluster"
+   recreate_remote_db=1  
+else 
+   echo "Local and remote database names match [$database_name]"
+fi
 if [ "${local_node_names[*]}" != "${remote_node_names[*]}" ]; then
-   echo "Cluster node names do not match.. configuring names on remote cluster"
+   echo "Cluster node names do not match. Recreate remote DB with matching names."
+   recreate_remote_db=1
+else 
+   echo "Local and remote node names match"
+fi
+if [ "$local_db_paths" != "$remote_db_paths" ]; then
+   echo "Local data/catalog path [$local_db_paths]. Remote data/catalog path [$remote_db_paths]"
+   echo "Cluster data/catalog paths do not match. Recreate remote DB with matching paths."
+   recreate_remote_db=1
+else
+   echo "Local and remote paths match"
+fi
+
+# recreate database on remote cluster with aligned databaes name, node names, and paths
+if [ $recreate_remote_db -eq 1 ]; then
+   echo "Recreating database [$database_name] on remote cluster [$autoscaling_group_name / $publicIp]"
    site_host_file="site_host.txt"; rm -f $site_host_file
    count=0
    # loop through each node name
    while [ ! -z "${local_node_names[count]}" ]
    do
-      # create site_host entry to map node names to remote cluster node
+      # create site_host entry to map node names and data/catalog paths to remote cluster node
       # Format: v_vmart_node0001 10.0.10.149 /vertica/data /vertica/data
-      site_host_mapping="${local_node_names[count]} ${remote_node_addrs[count]} $dbPath $dbPath"
+      site_host_mapping="${local_node_names[count]} ${remote_node_addrs[count]} $local_db_paths"
       echo "node_name mapping [$site_host_mapping]"
       echo $site_host_mapping >> $site_host_file
       count=$(( $count + 1 ))
    done
    node_list=$(cat $site_host_file | cut -d ' ' -f1 | paste -d, -s)
    scp $site_host_file $publicIp:/home/dbadmin/autoscale/site_host.txt
+   catalog_path=$(echo $local_db_paths | cut -d" " -f1)
+   data_path=$(echo $local_db_paths | cut -d" " -f2)
    ssh $publicIp "(
       cd ~/autoscale
-      echo "Stop and drop database [$database_name] on target cluster [$publicIp]"
-      admintools -t stop_db -d $database_name
-      admintools -t drop_db -d $database_name
-      echo "Configure remote node names"
+      echo "Check data and catalog paths"
+      for node in ${remote_node_addrs[*]}
+      do
+         echo "Check paths [$local_db_paths] on node [\$node]"
+         ssh -o \"StrictHostKeyChecking no\" \$node \"(
+            # Catalog Path
+            if [ -d $catalog_path ]; then 
+               echo "[\$node] Catalog path exists [$catalog_path]"
+            else
+               echo "[\$node] Catalog path does not exist. Create symbolic link from [$catalog_path] to [/vertica/data]"
+               sudo mkdir -p $(dirname $catalog_path)
+               sudo ln -s /vertica/data/ $catalog_path 
+            fi
+            sudo chown dbadmin $catalog_path
+            touch $catalog_path/writetest.tmp
+            if [ \$? -eq 0 ]; then
+               rm -f $catalog_path/writetest.tmp
+            else
+               echo "[\$node] ERROR: Unable to write test file to $catalog_path/writetest.tmp"
+               exit 1
+            fi
+            # Data Path
+            if [ -d $data_path ]; then
+               echo "[\$node] Data path exists [$data_path]"
+            else
+               echo "[\$node] Data path does not exist. Create symbolic link from [$data_path] to [/vertica/data]"
+               sudo mkdir -p $(dirname $data_path)
+               sudo ln -s /vertica/data/ $data_path 
+            fi
+            sudo chown dbadmin $data_path
+            touch $data_path/writetest.tmp
+            if [ \$? -eq 0 ]; then
+               rm -f $data_path/writetest.tmp
+            else
+               echo "[\$node] ERROR: Unable to write test file to $data_path/writetest.tmp"
+               exit 1
+            fi
+         )\"
+         [ \$? -ne 0 ] && exit 1
+      done
+      echo "Stop and drop database [$remote_db] on target cluster [$autoscaling_group_name / $publicIp]"
+      admintools -t stop_db -d $remote_db
+      admintools -t drop_db -d $remote_db
+      echo "Configure remote node names for database [$database_name]"
       admintools -u -t config_nodes -f ./site_host.txt -c
-      echo "Recreate remote database [$database_name] on named nodes [$node_list]"
+      echo "Create remote database [$database_name] on named nodes [$node_list]"
       admintools -u -t create_db -d $database_name -p $pwd -s $node_list --compat21
       echo "Install autoscaling features on remote database [$database_name]"
       sh ./database_configure.sh > /dev/null 2>&1
    )"
+   if [ $? -ne 0 ]; then
+      echo "Failed to create database on remote cluster"
+      exit 1
+   fi
    remote_node_names=($(vsql -h $publicIp $pwdArg -qAt -c "select node_name from nodes order by node_name"))
    if [ "${local_node_names[*]}" != "${remote_node_names[*]}" ]; then
       echo "Attempt to align remote cluster node names failed!"
@@ -132,9 +197,6 @@ if [ "${local_node_names[*]}" != "${remote_node_names[*]}" ]; then
       echo "Remote node names [${remote_node_names[*]}]"
       exit 1
    fi
-   echo "Remote node names sucessfully aligned with local cluster"
-else
-   echo "Node names of source and target clusters match."
 fi
 
 # create password file, and restrict to owner rw
